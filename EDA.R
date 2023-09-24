3d240283d9f3067d3165e5210fac222da5551ac9
Cyril P <cyrilpuyo.utpmp@gmail.com>
2023-09-24 21:10:43 +0200

    Formatted sections for improved clarity.

--- EDA.R
+++ EDA.R
@@ -18,12 +18,13 @@ library(corrplot)
 library(spatstat)
 library(igraph)
 ############################################ LOADING DATA ----
-# 1. DATA READING AND FORMATTING
-## 1.1. READING IN DATA ----
+# Raw measurements exported from QuPath
 raw_measurements <- read.table("Data/raw_measurements.tsv", header = TRUE, sep = "\t")
+
+# Raw associated clinical data
 responses <- read.csv("Data/Responses.csv", header = TRUE, sep = ",")
 
-## 1.2. FORMATTING FOR EDA ----
+# PROCESSING ----
 # Treatment response dataset
 responses <- responses[, -c(4, 6:8)] # Eliminate unnecessary columns
 colnames(responses) <- c("ID", "Age", "Sex", "Response") # Set name to columns
@@ -84,12 +85,12 @@ tif_index <- eda_df %>%
 
 tif_index$Matched_Indices <- sapply(tif_index$Images, function(images_list) which(SPIAT_tifs %in% images_list))
 
-############################################ EXPLORATORY DATA ANALYSIS (EDA) ----
-# Retrieve SPIAT image vector indexes based on ID ----
-ID <- 303 # 1101, 502, 303 ----
+############################################ EDA ----
+# Image retriever by ID ----
+ID <- 303 # Other interesting cases(1101, 502, 303) ----
 tif_index_retriever <- tif_index[tif_index$ID == ID, "Matched_Indices"][[1]]
 print(paste("SPIAT_tifs indexes for patient", ID, "are:", tif_index_retriever))
-# 1. IMAGES PER PATIENT ----
+############################################  SECTION 1 / Images and cells ----
 summary(responses)
 summary(eda_df[,c("DAPI", "PD1", "CD8", "CD3", "TIM3", "LAG3", "CK")])
 
@@ -104,7 +105,7 @@ ggplot(tif_index, aes(x = as.factor(Count))) +
 
 ## 1.1. NUMBER OF CELLS
 # 2. NUMBER OF CELLS
-## 1.1. VISUALS - QQplot of number of cells distribution(IMG & ID) ----
+# VISUALS - Cells per img and ID (QQ-plots) ----
 # Number of cells per image (DENSITY)
 eda_df %>%
   group_by(Image) %>%
@@ -143,19 +144,7 @@ responses %>%
 # Number of cells (QQplot)
 qqPlot(responses$n_cells, dist = "norm", xlab = "Quantiles", ylab = "Number of Cells", main = "Number of Cells Per Patient")
 
-## 1.2. VISUALS - Barplot of age distribution (ID) ----
-# Overall age distribution
-responses %>%
-  group_by(ID) %>%
-  ggplot(aes(x = Age)) +
-  geom_histogram(aes(y = ..density..), fill = "steelblue", color = "white", bins = 30) +
-  geom_density(aes(y = ..density..), color = "red") +  
-  labs(title = "Distribution of Age",
-       x = "Age",
-       y = "Count")
-
-# 2. AGE
-# 2. Age distribution ----
+############################################  SECTION 2 / Age ----
 age_stats <- list()
 age_stats$residuals <- residuals(aov(Age ~ Response, data=responses))
 shapiro.test(age_stats$residuals)
@@ -177,7 +166,19 @@ ggplot(responses, aes(x = Sex)) +
 
 
 # 3. SEX
-# 3. Sex distribution ----
+# VISUALS - Age per ID (Barplot) ----
+# Overall age distribution
+responses %>%
+  group_by(ID) %>%
+  ggplot(aes(x = Age)) +
+  geom_histogram(aes(y = ..density..), fill = "steelblue", color = "white", bins = 30) +
+  geom_density(aes(y = ..density..), color = "red") +  
+  labs(title = "Distribution of Age",
+       x = "Age",
+       y = "Count")
+
+# 2. AGE
+############################################  SECTION 3 / Sex ----
 # LaTeX: P(X=k) = \frac{\binom{N}{n} \binom{M}{k} \binom{N-M}{n-k}}{\binom{N}{n}}
 choose(31, 10) / choose(46, 10)
 
@@ -185,7 +186,7 @@ choose(31, 10) / choose(46, 10)
 age_fishertest <- fisher.test(table(responses$Sex, responses$Response))
 age_fishertest$p.value
 
-# 4. Marker intensity ----
+############################################  SECTION 4 / Marker intensity ----
 eda_by_ID <- eda_df %>%
   group_by(ID) %>%
   summarise(
@@ -202,21 +203,19 @@ eda_by_ID <- eda_by_ID %>%
   mutate(Response = responses$Response[match(ID, responses$ID)]) %>%
   select(ID, Response, everything())
 
-## 4.1. STATISTICS - Normality test ----
+# STATISTICS -  Shapiro, Levene, KW, Dunn ----
 markers <- c("PD1", "CD8", "CD3", "TIM3", "LAG3", "CK")
 
-# Shapiro-Wilk test for normality
+# Shapiro-Wilk
 shapiro_test_by_group <- function(marker) {
   eda_by_ID %>%
     group_by(Response) %>%
     summarise(p_value = shapiro.test(get(marker))$p.value) %>%
     mutate(marker = marker)
 }
-
 eda_mIntensity_shapiro_byID <- bind_rows(lapply(markers, shapiro_test_by_group))
 
-## 4.2. STATISTICS - Homoscedasticity and KW test ----
-# Levene's test for homogeneity
+# Levene and KW test
 test_for_marker <- function(marker) {
   
   # Levene's test
@@ -231,13 +230,10 @@ test_for_marker <- function(marker) {
                     kw_p_value = kw_p_val))
 }
 
-# Running tests and binding results
+# Run tests and bind results
 eda_mIntensity_levene_KWfdr <- bind_rows(lapply(markers, test_for_marker))
 
-# Applying Benjamini-Hochberg adjustment
-eda_mIntensity_levene_KWfdr$adj_kw_p_value <- p.adjust(eda_mIntensity_levene_KWfdr$kw_p_value, method = "BH")
-
-## 4.3. STATISTICS - Dunn test ----
+# CD3 Dunn test
 dunn_results <- dunnTest(CD3 ~ Response, data = eda_by_ID, method = "bh")
 
 eda_mIntensity_dunn <- data.frame(
@@ -247,33 +243,52 @@ eda_mIntensity_dunn <- data.frame(
   P.adj = dunn_results$res$`P.adj`
 )
 
-# Visualize
-p <- ggplot(eda_by_ID, aes(x = Response, y = CD3)) +
-  geom_violin() + 
-  geom_jitter(width = 0.2, size = 2, alpha = 0.6) +
-  labs(title = "Mean CD3 Intensity by Response Group", y = NULL, x = NULL) +
-  theme_minimal() +
-  theme(legend.position = "none")
-
-# Significance bars with star notations
-p + geom_signif(comparisons = list(c("PD", "PR"), c("PR", "SD")), 
-                y_position = c(max(eda_by_ID$CD3) * 1.08, max(eda_by_ID$CD3) * 1.14), 
-                map_signif_level = TRUE)
-
-# Sorting
-eda_mIntensity_shapiro_byID <- eda_mIntensity_shapiro_byID %>%
-  arrange(p_value)
-print(eda_mIntensity_shapiro_byID)
+ggplot(eda_mIntensity_dunn, aes(x = reorder(Comparison, -Z), y = Z)) +
+  geom_bar(stat = "identity", aes(fill = P.adj <= 0.05)) + 
+  geom_text(aes(label = sprintf("%.2f", P.adj)), vjust = -0.5) +
+  scale_fill_manual(values = c("red", "gray"), 
+                    name = "Significance", 
+                    labels = c("p <= 0.05", "p > 0.05")) +
+  labs(title = "Dunn's Test Results for CD3",
+       x = "Group Comparison",
+       y = "Z-Score") +
+  theme_minimal()
 
 eda_mIntensity_levene_KWfdr <- eda_mIntensity_levene_KWfdr %>%
-  arrange(adj_kw_p_value)
+  arrange(kw_p_value)
 print(eda_mIntensity_levene_KWfdr)
 
 eda_mIntensity_dunn <- eda_mIntensity_dunn %>%
   arrange(P.adj)
 print(eda_mIntensity_dunn)
 
-## 4.4. VISUALS - Results ----
+# CD8 Dunn test
+dunn_results <- dunnTest(CD8 ~ Response, data = eda_by_ID, method = "bh")
+
+eda_mIntensity_dunn <- data.frame(
+  Comparison = dunn_results$res$Comparison,
+  Z = dunn_results$res$Z,
+  P.unadj = dunn_results$res$`P.unadj`,
+  P.adj = dunn_results$res$`P.adj`
+)
+
+eda_mIntensity_dunn <- eda_mIntensity_dunn %>%
+  arrange(P.adj)
+
+ggplot(eda_mIntensity_dunn, aes(x = reorder(Comparison, -Z), y = Z)) +
+  geom_bar(stat = "identity", aes(fill = P.adj <= 0.05)) + 
+  geom_text(aes(label = sprintf("%.2f", P.adj)), vjust = -0.5) +
+  scale_fill_manual(values = c("red", "gray"), 
+                    name = "Significance", 
+                    labels = c("p <= 0.05", "p > 0.05")) +
+  labs(title = "Dunn's Test Results for CD8",
+       x = "Group Comparison",
+       y = "Z-Score") +
+  theme_minimal()
+
+print(eda_mIntensity_dunn)
+
+# VISUALS - Results ----
 # Correlation plot by ID
 combined_data <- merge(eda_df, responses, by = "ID")
 
@@ -493,7 +508,7 @@ merged_df <- merge(eda_df, img_threshs, by="Image", all.x = TRUE)
 names(merged_df)[names(merged_df) == "ID.x"] <- "ID"
 merged_df$ID.y <- NULL
 
-# 2. PROCESSING - Applying cutoff ----
+# PROCESSING - Applying cutoff ----
 immune_markers <- c("PD1", "CD8", "CD3", "TIM3", "LAG3", "CK")
 
 for(marker in immune_markers) {
@@ -501,7 +516,7 @@ for(marker in immune_markers) {
   merged_df[, marker] <- ifelse(merged_df[, marker] >= merged_df[, cutoff_col], 1, 0)
 }
 
-## 2.1. PROCESSING - Handling superposition ---- 
+# PROCESSING - Handling superposition ---- 
 # Add a column for duplicate identification
 # CK adjustment
 result <- pbapply(merged_df, 1, function(row) {
@@ -540,7 +555,7 @@ final_df <- rbind.data.frame(original_rows, duplicate_rows)
 final_df <- final_df[, c("Image", "X", "Y", "ID", "DAPI", immune_markers)]
 final_df[,-1] <- lapply(final_df[,-1], as.numeric)
 
-## 2.2. PROCESSING - Assigning phenotypes ----
+# PROCESSING - Assigning phenotypes ----
 # Immune_markers vector
 immune_markers <- c("PD1", "CD8", "CD3", "TIM3", "LAG3", "CK")
 
@@ -563,9 +578,9 @@ random <- function() {
 # Remove intermediate variables to clear memory
 rm(cutoff_col, duplicate_rows, merged_df, original_rows, original_cols, result, phenotype_strings)
 
-# 3. VISUALS - Phenotype dotplot (IMAGES) ----
+# VISUALS - Predicted phenotypes (Dotplot) ----
 selected_image <- SPIAT_tifs[random()] # 1 to 90 possible images ----
-selected_image <- SPIAT_tifs[65]
+selected_image <- SPIAT_tifs[65] # Select a specific image
 # Join final_df and responses
 final_df_joined <- final_df %>%
   left_join(responses, by = "ID")
@@ -589,7 +604,7 @@ p <- final_df_joined %>%
 
 ggplotly(p)
 
-## 3.1. VISUALS - Marker distribution and tresholding (IMAGES) ----
+# VISUALS - Cutoff quality (IMAGES) ----
 
 long_eda_df <- eda_df[eda_df$Image == selected_image, ] %>%
   pivot_longer(cols = colnames(eda_df[, 6:11]), names_to = "marker", values_to = "intensity")
@@ -620,7 +635,7 @@ eda_df %>%
   theme_minimal()
 
 
-## 3.2. VISUALS - Median phenotype proportion per response group (RESPONSE) ----
+# VISUALS - Phenotype proportion per response group (Stacked Barplot) ----
 # Median Proportion of Phenotypes per Response Group (PATIENTS)
 # Step 1: Get unique phenotypes across all images
 cp_unique_phenos <- unique(final_df$Phenotype)
@@ -755,39 +770,81 @@ ggplotly(cp_plot)
 # Matrix products: default
 # 
 # locale:
-# [1] LC_COLLATE=English_United States.utf8  LC_CTYPE=English_United States.utf8    LC_MONETARY=English_United States.utf8
-# [4] LC_NUMERIC=C                           LC_TIME=English_United States.utf8    
+# [1] LC_COLLATE=English_United States.utf8  LC_CTYPE=English_United States.utf8   
+# [3] LC_MONETARY=English_United States.utf8 LC_NUMERIC=C                          
+# [5] LC_TIME=English_United States.utf8    
 # 
 # attached base packages:
-# [1] stats4    stats     graphics  grDevices utils     datasets  methods   base     
+# [1] stats4    stats     graphics  grDevices utils     datasets  methods  
+# [8] base     
 # 
 # other attached packages:
-#  [1] spatstat_3.0-6              spatstat.linnet_3.1-1       spatstat.model_3.2-4        rpart_4.1.19                spatstat.explore_3.2-1     
-#  [6] nlme_3.1-162                spatstat.random_3.1-5       spatstat.geom_3.2-1         spatstat.data_3.0-1         corrplot_0.92              
-# [11] ggsignif_0.6.4              FSA_0.9.4                   ggridges_0.5.4              pbapply_1.7-0               moments_0.14.1             
-# [16] car_3.1-2                   carData_3.0-5               readxl_1.4.2                reshape2_1.4.4              BBmisc_1.13                
-# [21] plotly_4.10.2               gridExtra_2.3               lubridate_1.9.2             forcats_1.0.0               stringr_1.5.0              
-# [26] dplyr_1.1.2                 purrr_1.0.1                 readr_2.1.4                 tidyr_1.3.0                 tibble_3.2.1               
-# [31] ggplot2_3.4.3               tidyverse_2.0.0             SPIAT_1.0.4                 SpatialExperiment_1.8.1     SingleCellExperiment_1.20.1
-# [36] SummarizedExperiment_1.28.0 Biobase_2.58.0              GenomicRanges_1.50.2        GenomeInfoDb_1.34.9         IRanges_2.32.0             
-# [41] S4Vectors_0.36.2            BiocGenerics_0.44.0         MatrixGenerics_1.10.0       matrixStats_0.63.0         
+# [1] igraph_1.5.1                spatstat_3.0-6             
+# [3] spatstat.linnet_3.1-1       spatstat.model_3.2-4       
+# [5] rpart_4.1.19                spatstat.explore_3.2-1     
+# [7] nlme_3.1-162                spatstat.random_3.1-5      
+# [9] spatstat.geom_3.2-1         spatstat.data_3.0-1        
+# [11] corrplot_0.92               ggsignif_0.6.4             
+# [13] FSA_0.9.4                   ggridges_0.5.4             
+# [15] pbapply_1.7-0               moments_0.14.1             
+# [17] car_3.1-2                   carData_3.0-5              
+# [19] readxl_1.4.2                reshape2_1.4.4             
+# [21] BBmisc_1.13                 plotly_4.10.2              
+# [23] gridExtra_2.3               lubridate_1.9.2            
+# [25] forcats_1.0.0               stringr_1.5.0              
+# [27] dplyr_1.1.2                 purrr_1.0.1                
+# [29] readr_2.1.4                 tidyr_1.3.0                
+# [31] tibble_3.2.1                ggplot2_3.4.3              
+# [33] tidyverse_2.0.0             SPIAT_1.0.4                
+# [35] SpatialExperiment_1.8.1     SingleCellExperiment_1.20.1
+# [37] SummarizedExperiment_1.28.0 Biobase_2.58.0             
+# [39] GenomicRanges_1.50.2        GenomeInfoDb_1.34.9        
+# [41] IRanges_2.32.0              S4Vectors_0.36.2           
+# [43] BiocGenerics_0.44.0         MatrixGenerics_1.10.0      
+# [45] matrixStats_0.63.0         
 # 
 # loaded via a namespace (and not attached):
-#  [1] colorspace_2.1-0          rjson_0.2.21              deldir_1.0-9              ellipsis_0.3.2            scuttle_1.8.4            
-#  [6] XVector_0.38.0            rstudioapi_0.14           farver_2.1.1              fansi_1.0.4               splines_4.2.2            
-# [11] codetools_0.2-19          R.methodsS3_1.8.2         sparseMatrixStats_1.10.0  dunn.test_1.3.5           polyclip_1.10-4          
-# [16] jsonlite_1.8.4            R.oo_1.25.0               HDF5Array_1.26.0          spatstat.sparse_3.0-1     compiler_4.2.2           
-# [21] httr_1.4.6                dqrng_0.3.0               backports_1.4.1           Matrix_1.5-4.1            fastmap_1.1.1            
-# [26] lazyeval_0.2.2            limma_3.54.2              cli_3.6.1                 htmltools_0.5.6           tools_4.2.2              
-# [31] gtable_0.3.4              glue_1.6.2                GenomeInfoDbData_1.2.9    Rcpp_1.0.10               cellranger_1.1.0         
-# [36] vctrs_0.6.3               rhdf5filters_1.10.1       crosstalk_1.2.0           DelayedMatrixStats_1.20.0 beachmat_2.14.2          
-# [41] timechange_0.2.0          lifecycle_1.0.3           goftest_1.2-3             edgeR_3.40.2              zlibbioc_1.44.0          
-# [46] scales_1.2.1              hms_1.1.3                 spatstat.utils_3.0-3      parallel_4.2.2            rhdf5_2.42.1             
-# [51] yaml_2.3.7                stringi_1.7.12            checkmate_2.2.0           BiocParallel_1.32.6       rlang_1.1.1              
-# [56] pkgconfig_2.0.3           bitops_1.0-7              pracma_2.4.2              lattice_0.20-45           tensor_1.5               
-# [61] Rhdf5lib_1.20.0           labeling_0.4.2            htmlwidgets_1.6.2         tidyselect_1.2.0          plyr_1.8.8               
-# [66] magrittr_2.0.3            R6_2.5.1                  magick_2.7.4              generics_0.1.3            DelayedArray_0.23.2      
-# [71] mgcv_1.8-42               pillar_1.9.0              withr_2.5.0               abind_1.4-5               RCurl_1.98-1.12          
-# [76] crayon_1.5.2              DropletUtils_1.18.1       utf8_1.2.3                tzdb_0.4.0                locfit_1.5-9.7           
-# [81] grid_4.2.2                data.table_1.14.8         digest_0.6.33             R.utils_2.12.2            munsell_0.5.0            
-# [86] viridisLite_0.4.2        
+# [1] colorspace_2.1-0          rjson_0.2.21             
+# [3] deldir_1.0-9              ellipsis_0.3.2           
+# [5] scuttle_1.8.4             XVector_0.38.0           
+# [7] rstudioapi_0.14           farver_2.1.1             
+# [9] scico_1.5.0               fansi_1.0.4              
+# [11] splines_4.2.2             codetools_0.2-19         
+# [13] R.methodsS3_1.8.2         sparseMatrixStats_1.10.0 
+# [15] dunn.test_1.3.5           polyclip_1.10-4          
+# [17] jsonlite_1.8.4            R.oo_1.25.0              
+# [19] HDF5Array_1.26.0          spatstat.sparse_3.0-1    
+# [21] compiler_4.2.2            httr_1.4.6               
+# [23] dqrng_0.3.0               backports_1.4.1          
+# [25] Matrix_1.5-4.1            fastmap_1.1.1            
+# [27] lazyeval_0.2.2            limma_3.54.2             
+# [29] cli_3.6.1                 htmltools_0.5.6          
+# [31] tools_4.2.2               gtable_0.3.4             
+# [33] glue_1.6.2                GenomeInfoDbData_1.2.9   
+# [35] Rcpp_1.0.10               cellranger_1.1.0         
+# [37] vctrs_0.6.3               rhdf5filters_1.10.1      
+# [39] crosstalk_1.2.0           DelayedMatrixStats_1.20.0
+# [41] beachmat_2.14.2           timechange_0.2.0         
+# [43] lifecycle_1.0.3           goftest_1.2-3            
+# [45] edgeR_3.40.2              zlibbioc_1.44.0          
+# [47] scales_1.2.1              hms_1.1.3                
+# [49] spatstat.utils_3.0-3      parallel_4.2.2           
+# [51] rhdf5_2.42.1              yaml_2.3.7               
+# [53] stringi_1.7.12            checkmate_2.2.0          
+# [55] BiocParallel_1.32.6       rlang_1.1.1              
+# [57] pkgconfig_2.0.3           bitops_1.0-7             
+# [59] pracma_2.4.2              lattice_0.20-45          
+# [61] tensor_1.5                Rhdf5lib_1.20.0          
+# [63] labeling_0.4.2            htmlwidgets_1.6.2        
+# [65] tidyselect_1.2.0          plyr_1.8.8               
+# [67] magrittr_2.0.3            R6_2.5.1                 
+# [69] magick_2.7.4              generics_0.1.3           
+# [71] DelayedArray_0.23.2       mgcv_1.8-42              
+# [73] pillar_1.9.0              withr_2.5.0              
+# [75] abind_1.4-5               RCurl_1.98-1.12          
+# [77] crayon_1.5.2              DropletUtils_1.18.1      
+# [79] utf8_1.2.3                tzdb_0.4.0               
+# [81] locfit_1.5-9.7            grid_4.2.2               
+# [83] data.table_1.14.8         digest_0.6.33            
+# [85] R.utils_2.12.2            munsell_0.5.0            
+# [87] viridisLite_0.4.2
